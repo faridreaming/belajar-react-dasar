803329465fd111fcd98f1034870e824142c1ce6b
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-02-09 21:08:03 +0700

    feat: manipulate dom using ref

---
+++ src/guestbook/GuestBook.jsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from 'react'
+import GuestBookForm from './GuestBookForm'
+
+export default function GuestBook() {
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+
+  const nameInput = useRef(null)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setName('')
+    setMessage('')
+
+    nameInput.current.focus()
+
+    alert(`Name : ${name}\nMessage : ${message}`)
+  }
+
+  return (
+    <>
+      <h1>Guest Book</h1>
+      <form>
+        <GuestBookForm ref={nameInput} name={name} setName={setName} />
+        <br />
+        <label htmlFor="message">Message</label>
+        <br />
+        <textarea
+          type="text"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <br />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  )
+}
