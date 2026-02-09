fb4d81d4a83dd95c3c39fca200efe39cf4617f16
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-02-09 15:59:44 +0700

    feat: change reducer to immer reducer

--- src/note/NoteApp.jsx
+++ src/note/NoteApp.jsx
@@ -1,6 +1,6 @@
+import { useImmerReducer } from 'use-immer'
 import NoteForm from './NoteForm'
 import NoteList from './NoteList'
-import { useReducer } from 'react'
 
 let id = 0
 const initialNotes = [
@@ -26,32 +26,25 @@ const initialNotes = [
   },
 ]
 
-function noteReducer(notes, action) {
-  switch (action.type) {
-    case 'ADD_NOTE':
-      return [
-        ...notes,
-        {
-          id: id++,
-          text: action.text,
-          done: false,
-        },
-      ]
-    case 'CHANGE_NOTE':
-      return notes.map((note) =>
-        note.id === action.id
-          ? { ...note, text: action.text, done: action.done }
-          : note,
-      )
-    case 'DELETE_NOTE':
-      return notes.filter((note) => note.id !== action.id)
-    default:
-      return notes
+function noteReducer(draft, action) {
+  if (action.type === 'ADD_NOTE') {
+    draft.push({
+      id: id++,
+      text: action.text,
+      done: false,
+    })
+  } else if (action.type === 'CHANGE_NOTE') {
+    const index = draft.findIndex((note) => note.id === action.id)
+    draft[index].text = action.text
+    draft[index].done = action.done
+  } else if (action.type === 'DELETE_NOTE') {
+    const index = draft.findIndex((note) => note.id === action.id)
+    draft.splice(index, 1)
   }
 }
 
 export default function NoteApp() {
-  const [notes, dispatch] = useReducer(noteReducer, initialNotes)
+  const [notes, dispatch] = useImmerReducer(noteReducer, initialNotes)
 
   function handleAddNote(text) {
     dispatch({
