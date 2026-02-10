853c9a3a76abc7b329024e78c3abb9a9ab1d73c7
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-02-10 08:25:40 +0700

    feat: use async awain in use effect

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,27 +1,38 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Product from './Product'
 
 export default function ProductList() {
   const [products, setProducts] = useState([])
-  const loaded = useRef(false)
+  const [loaded, setLoaded] = useState(false)
+
+  function handleClick() {
+    setLoaded(true)
+  }
 
   useEffect(() => {
-    console.log('Call useEffect')
-    if (!loaded.current) {
-      fetch('/products.json')
-        .then((response) => response.json())
-        .then((data) => setProducts(data))
-        .then(() => (loaded.current = true))
+    console.info('Ini yus ifek dari line 13 :v')
+  }, [])
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const response = await fetch('/products.json')
+      const data = await response.json()
+      setProducts(data)
+    }
+
+    if (loaded) {
+      fetchProduct()
     }
 
     return () => {
       console.info('Component unmounted')
     }
-  })
+  }, [loaded])
 
   return (
     <>
       <h1>Product List</h1>
+      <button onClick={handleClick}>Load Products</button>
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
